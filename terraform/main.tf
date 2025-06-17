b578a9edf7834ed53cf05c0658c154fa3c37fc22
Sergei Chesnokov <ches.tranveks@gmail.com>
2025-06-17 14:47:57 -0400

    Initial OpenWebUI Helm deploy-2

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,11 +36,11 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
 }
 
 resource "aws_lambda_function" "lambda_func" {
-  function_name    = "MyLambdaFunction"
-  role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "main.lambda_handler"
-  runtime          = "python3.9"
-  timeout          = 10
+  function_name = "MyLambdaFunction"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "main.lambda_handler"
+  runtime       = "python3.9"
+  timeout       = 10
 
   # Файл zip должен содержать:
   # - main.py
