2b9d0ad48f2caed07c45b5d9aad25da8c1d46b83
Sergei Chesnokov <ches.tranveks@gmail.com>
2025-06-17 16:11:04 -0400

    Initial OpenWebUI Helm deploy-3

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5"
+      version = "~>5"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5"
+      version = "~>3.5"
     }
+  }
+  required_version = ">= 1.8"
+  #### enable remote TF state
+  backend "s3" {
+    bucket = "terraform-state-s3-sergei" # please use your bucket name!!!
+    key    = "aws-lambda/tf-state.tfstate"       # Path to the state file in the bucket
+    region = "us-east-1"
   }
 }
 
